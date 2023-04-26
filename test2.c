549ec1639ce68e2067dc08ba1bfeaacbaf341fc1
Moses <mosemuthoni57@gmail.com>
2023-04-26 16:33:02 -0700

    printing

--- test2.c
+++ test2.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- *
- *
+ * check_arg - a functio that checks for arguments
+ * @format: the passed types
+ * Return: 1 if success
  */
-int check_arg(const char* format)
+int check_arg(const char *format)
 {
 	int i = 0, j = 0;
+
 	while (format[i])
 	{
 		if (format[i] == '%')
