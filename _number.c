e866a86086f8dc5d9e3682a68e053fa204ef8c24
Moses <mosemuthoni57@gmail.com>
2023-04-28 11:20:22 -0700

    printing

---
+++ _number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * number - a function that prints a number character
+ * @a: the number to be printed
+ * Return: the characters printed
+ */
+int number(int a)
+{
+	char s2;
+	char *s;
+	int j, i = 0;
+
+	if (a == INT_MIN)
+	{
+		s = "-2147483648";
+		for (i = 0; s[i] != '\0';  i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+	if (a < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+	s = malloc(sizeof(a));
+	do {
+		s[i++] = a % 10 + '0';
+		a /= 10;
+	} while (a > 0);
+	s[i] = '\0';
+	for (j = 0; j < i / 2; j++)
+	{
+		s2 =  s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = s2;
+	}
+	i = 0;
+	j = 0;
+	while (s[j] != '\0')
+	{
+		_putchar(s[j++]);
+		i++;
+	}
+	free(s);
+	return (i);
+}
